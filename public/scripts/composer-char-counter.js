50544f8f23720ad5030ac02d693fef352f361b46
Calvin Chung <calvin-chung@live.com>
2022-03-29 18:48:28 -0700

    set fixed navbar

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+  
+  //textarea 
+  $('#tweet-text').on('input', function() {
+    const inputValue = $(this).val().length;
+    $(".counter").val(140 - inputValue);
+    
+    if (inputValue > 140) {
+      document.getElementById("tweet-counter").className="counter counter-red";
+    } else if (inputValue <= 140) {
+      document.getElementById("tweet-counter").className="counter";
+    }
+    
+  });
+  
+  const $tweetButton = $('#tweet-button');
+  $tweetButton.on('click', () => {
+    console.log("clicked");
+  })
+
+});
+
